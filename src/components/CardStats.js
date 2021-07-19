8331b011af87472442cd891be7c51f8633c7b669
sharathrdev <sharathrdev@gmail.com>
2021-07-19 12:44:17 -0500

    Completed  design for Card component to display stats. Added constnt file

--- src/components/CardStats.js
+++ src/components/CardStats.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { arrColors } from '../utils/constants';
 
-const  CardStats = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    color: '#ffffff',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+}));
 
 
-    return(
-        <div> Card Stats presentational component </div>
-    )
-}
+
+  return (
+    <Card className={classes.root} style={{ backgroundColor: `${arrColors[props.index]}` }}>
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            Units
+          </Typography>
+          <Typography variant="subtitle1">Heading</Typography>
+        </CardContent>
+      </div>
+    </Card>
+  );
+};
 
 export default CardStats;
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+
+export const arrColors = ['#007bff', '#e83e8c', '#ffc107', '#28a745', '#fd7e14', '#6f42c1'];
